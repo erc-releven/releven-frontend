63ae803671404272438bbc0e8488f197d1b09a14
Kevin Stadler <kevinstadler@users.noreply.github.com>
2025-06-13 11:31:10 +0200

    feat: add styled index page

---
+++ app/[locale]/(index)/imagecard.tsx
@@ -0,0 +1,42 @@
+import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { useTranslations } from "next-intl";
+import type { ReactNode } from "react";
+
+import { ServerImage } from "@/components/server-image";
+import { Button } from "@/components/ui/button";
+
+interface ImageCardProps {
+	icon: string | StaticImport;
+	image: string | StaticImport;
+	title: string;
+	subtitle: string;
+	description: string;
+}
+
+export function ImageCard(props: Readonly<ImageCardProps>): ReactNode {
+	const t = useTranslations("IndexPage");
+	return (
+		<div className="grid w-[19rem] grid-cols-1 grid-rows-[4.5rem_12rem_1fr_3rem] overflow-hidden border border-gray-500 text-ellipsis">
+			<div className="flex flex-row items-center justify-start gap-4 p-4">
+				<ServerImage
+					alt=""
+					className="size-10 rounded-full bg-[#0000001e] p-2"
+					height={40}
+					src={props.icon}
+					width={40}
+				/>
+				<div className="w-full text-nowrap">
+					<span className="block text-lg font-medium">{props.title}</span>
+					<span className="block">{props.subtitle}</span>
+				</div>
+			</div>
+			<div className="relative">
+				<ServerImage alt="" className="object-cover" fill={true} src={props.image} />
+			</div>
+			<div className="p-4">{props.description}</div>
+			<div className="relative w-full">
+				<Button className="absolute right-2 bottom-3 uppercase">{t("try_it_out")}</Button>
+			</div>
+		</div>
+	);
+}
--- app/[locale]/(index)/page.tsx
+++ app/[locale]/(index)/page.tsx
@@ -1,14 +1,24 @@
-import { ArrowRightIcon } from "lucide-react";
 import type { Metadata, ResolvingMetadata } from "next";
 import { useTranslations } from "next-intl";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import type { ReactNode } from "react";
 
 import { Image } from "@/components/image";
-import { Link } from "@/components/link";
-import { Logo } from "@/components/logo";
 import { MainContent } from "@/components/ui/main-content";
 import type { IntlLocale } from "@/lib/i18n/locales";
+import discoverIcon from "@/public/assets/images/candle.png";
+import clarin from "@/public/assets/images/clarin.png";
+import coretrust from "@/public/assets/images/core-trust-seal.png";
+import leaderImage from "@/public/assets/images/leader.png";
+import searchIcon from "@/public/assets/images/magnifier.png";
+import personImage from "@/public/assets/images/person.png";
+import placeImage from "@/public/assets/images/place.png";
+import researchPlatformImage from "@/public/assets/images/researchplatform.png";
+import compareIcon from "@/public/assets/images/scale.png";
+import textImage from "@/public/assets/images/text.png";
+
+import { IconCard } from "./iconcard";
+import { ImageCard } from "./imagecard";
 
 interface IndexPageProps {
 	params: Promise<{
@@ -45,103 +55,111 @@ export default async function IndexPage(props: Readonly<IndexPageProps>): Promis
 	setRequestLocale(locale);
 
 	return (
-		<MainContent className="layout-grid content-start">
-			<HeroSection />
-			<FeaturesSection />
+		<MainContent>
+			<IntroSection />
+			<ResearchPlatformSection />
+			<AboutSection />
+			<ViewpointSection />
 		</MainContent>
 	);
 }
 
-function HeroSection(): ReactNode {
+function IntroSection(): ReactNode {
 	const t = useTranslations("IndexPage");
 
 	return (
-		<section className="relative layout-subgrid gap-y-10 bg-fill-weaker py-16 xs:py-24">
-			<div className="grid max-w-text gap-y-6">
-				<span className="inline-flex items-center gap-x-2 justify-self-start rounded-4 border border-stroke-weak bg-fill-weak px-3 py-0.5 text-tiny text-text-strong">
-					<Logo className="size-5 shrink-0 text-icon-neutral" />
-					<span>{t("badge")}</span>
-				</span>
-				<h1 className="font-heading text-display font-strong text-balance text-text-strong">
+		<section className="mx-auto flex w-full flex-col items-center gap-3 bg-[#FBF7F0] md:py-12 lg:flex-row">
+			<div className="flex max-w-screen-lg basis-1/3 flex-col gap-6 px-8 py-5">
+				<h1 className="text-3xl leading-tight font-bold tracking-tighter text-balance text-primary md:text-5xl lg:text-6xl">
 					{t("title")}
 				</h1>
-				<p className="font-heading text-small text-text-weak xs:text-heading-4">{t("lead-in")}</p>
+				<div className="w-full max-w-screen-md sm:text-xl">{t("lead-in")}</div>
+				<input
+					className="rounded-full border border-solid border-gray-50 p-3"
+					placeholder={t("search_placeholder")}
+					type="search"
+				/>
+				<div className="flex flex-row gap-10 px-8">
+					<Image alt="" src={clarin} />
+					<Image alt="" src={coretrust} />
+				</div>
+			</div>
+			<div className="basis-2/3">
+				<Image alt="" className="object-cover" height={358} src={leaderImage} />
 			</div>
 		</section>
 	);
 }
 
-function FeaturesSection(): ReactNode {
-	const _t = useTranslations("IndexPage");
+function ResearchPlatformSection(): ReactNode {
+	const t = useTranslations("IndexPage.researchplatform");
 
-	const features = {
-		i18n: { title: "Internationalisation" },
-		e2e: { title: "End-to-end tests" },
-		"dark-mode": { title: "Dark mode" },
-		cms: { title: "Content management system" },
-		feed: { title: "RSS Feed" },
-		authentication: { title: "Authentication" },
+	const cards = {
+		people: { icon: personImage },
+		places: { icon: placeImage },
+		texts: { icon: textImage },
 	};
 
-	/* eslint-disable react/jsx-no-literals */
 	return (
-		<section className="relative layout-subgrid gap-y-12 border-t border-stroke-weak py-16 xs:py-24">
-			<header className="grid max-w-text gap-y-4">
-				<h2 className="font-heading text-heading-2 font-strong text-balance text-text-strong">
-					Features
-				</h2>
-				<p className="font-heading text-heading-4 text-text-weak">
-					This template comes with important features built in.
-				</p>
-			</header>
-
-			<ul
-				className="grid grid-cols-[repeat(auto-fill,minmax(min(100%,20rem),1fr))] gap-8"
-				role="list"
-			>
-				{Object.entries(features).map(([id, feature]) => {
+		<section className="flex flex-col items-center py-20">
+			<h2 className="text-lg font-extrabold text-primary uppercase">{t("title")}</h2>
+			<span className="mb-20 text-3xl">{t("lead-in")}</span>
+			<div className="flex flex-col items-center gap-4 lg:flex-row">
+				{Object.entries(cards).map(([id, card]) => {
 					return (
-						<li key={id}>
-							<article className="grid grid-rows-[14rem_auto] overflow-hidden rounded-4 border border-stroke-weak bg-background-raised shadow-raised">
-								<Image
-									alt=""
-									className="size-full border-b border-stroke-weak object-cover"
-									height={300}
-									/** Preload image because it's the largest contentful paint (lcp) element. */
-									priority={true}
-									src="https://picsum.photos/400/300"
-									unoptimized={true}
-									width={400}
-								/>
-								<div className="grid gap-y-6 p-8">
-									<div className="grid gap-y-2">
-										<h3 className="font-heading text-heading-4 font-strong text-text-strong">
-											{feature.title}
-										</h3>
-										<p className="text-small text-text-weak">
-											Excepteur eiusmod dolor eu ut nulla cillum nisi irure proident. Reprehenderit
-											irure voluptate ex consectetur magna quis aute quis eiusmod sunt in in elit.
-										</p>
-									</div>
-								</div>
-							</article>
-						</li>
+						<ImageCard
+							key={id}
+							description={t(`${id}.description`)}
+							icon={`/assets/images/${id}.svg`}
+							image={card.icon}
+							subtitle={t(`${id}.subtitle`)}
+							title={t(`${id}.title`)}
+						/>
 					);
 				})}
-			</ul>
+			</div>
+		</section>
+	);
+}
 
-			<div>
-				<Link
-					className="group inline-flex items-center gap-x-2 rounded-0.5 text-small text-text-brand outline-transparent focus-visible:focus-outline"
-					href="/"
-				>
-					<span className="underline group-hover:no-underline">See all</span>
-					<ArrowRightIcon
-						aria-hidden={true}
-						className="size-5 shrink-0 text-icon-brand transition group-hover:translate-x-1"
-						data-slot="icon"
-					/>
-				</Link>
+function AboutSection() {
+	const _t = useTranslations("IndexPage.about");
+	return (
+		<section className="flex flex-col items-center bg-[#CEA46F] p-10">
+			<h2 className="text-lg font-extrabold text-primary uppercase">{_t("title")}</h2>
+			<span className="block text-center text-3xl">{_t("lead-in")}</span>
+			<div className="flex flex-col items-start gap-8 py-16 lg:flex-row">
+				<Image alt="" className="max-w-fit" src={researchPlatformImage} />
+				<div className="max-w-[31rem]">
+					<p>{_t("description")}</p>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+function ViewpointSection() {
+	const _t = useTranslations("IndexPage.services");
+	const cards = {
+		related: { icon: searchIcon },
+		viewpoints: { icon: compareIcon },
+		relationships: { icon: discoverIcon },
+	};
+	return (
+		<section className="flex flex-col items-center py-16">
+			<h2 className="text-lg font-extrabold text-primary uppercase">{_t("title")}</h2>
+			<span className="block text-center text-3xl">{_t("lead-in")}</span>
+			<div className="flex flex-col gap-8 py-16 lg:flex-row">
+				{Object.entries(cards).map(([id, card]) => {
+					return (
+						<IconCard
+							key={id}
+							description={_t(`${id}.description` as never)}
+							icon={card.icon}
+							title={_t(`${id}.title` as never)}
+						/>
+					);
+				})}
 			</div>
 		</section>
 	);
