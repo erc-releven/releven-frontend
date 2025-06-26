3342c5366d029547768689f0be9ed4b9427d1f55
Kevin Stadler <kevinstadler@users.noreply.github.com>
2025-06-26 14:27:34 +0200

    feat: implement catch-all markdown content rendering

--- app/[locale]/[...segments]/page.tsx
+++ app/[locale]/[...segments]/page.tsx
@@ -1,5 +1,27 @@
+import { join } from "node:path";
+
 import { notFound } from "next/navigation";
 import type { ReactNode } from "react";
+import rehypeExternalLinks from "rehype-external-links";
+import toHtml from "rehype-stringify";
+import fromMarkdown from "remark-parse";
+import toHast from "remark-rehype";
+import { read } from "to-vfile";
+import { unified } from "unified";
+
+import { MainContent } from "@/components/ui/main-content";
+
+const processor = unified()
+	.use(fromMarkdown)
+	.use(toHast)
+	.use(rehypeExternalLinks, { target: "_blank" })
+	.use(toHtml);
+
+interface StaticPageProps {
+	params: Promise<{
+		segments: Array<string>;
+	}>;
+}
 
 /**
  * Only a root `not-found.tsx` automatically handles unmatched URLs.
@@ -7,6 +29,21 @@ import type { ReactNode } from "react";
  *
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/not-found
  */
-export default function NotFoundPage(): Promise<ReactNode> {
-	notFound();
+export default async function StaticPage(props: Readonly<StaticPageProps>): Promise<ReactNode> {
+	const { segments } = await props.params;
+	try {
+		// TODO add support for locales
+		const filePath = join(process.cwd(), "content", `${segments.join("/")}.md`);
+
+		const vfile = await read(filePath);
+		const result = await processor.process(vfile);
+		return (
+			<MainContent className="m-auto w-2xl">
+				<section dangerouslySetInnerHTML={{ __html: String(result) }} className="prose" />
+			</MainContent>
+		);
+		/* eslint-disable @typescript-eslint/no-unused-vars */
+	} catch (error) {
+		notFound();
+	}
 }
