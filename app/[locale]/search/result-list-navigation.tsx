3b4d5800d40d5c95b373ee7b079133d99c4f0cdc
Kevin Stadler <kevinstadler@users.noreply.github.com>
2025-07-14 14:12:44 +0200

    feat: add search result pagination using react-paginate

--- app/[locale]/search/result-list-navigation.tsx
+++ app/[locale]/search/result-list-navigation.tsx
@@ -1,9 +1,12 @@
+"use client";
 import type { ReactNode } from "react";
+import ReactPaginate from "react-paginate";
 
 import { ListBox, ListBoxItem } from "@/components/ui/listbox";
 import { Popover } from "@/components/ui/popover";
 import { Select, SelectTrigger } from "@/components/ui/select";
 import type { SearchRecordResult } from "@/lib/model";
+import { usePathname, useRouter, useSearchParams } from "@/lib/navigation/navigation";
 
 interface ResultListNavigationProps {
 	data: SearchRecordResult;
@@ -14,14 +17,41 @@ interface ResultListNavigationProps {
 export function ResultListNavigation(props: Readonly<ResultListNavigationProps>): ReactNode {
 	const { data } = props;
 	const sort = ["name", "relevance"]; // TODO
+
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+	const params = new URLSearchParams(searchParams);
+	const { push } = useRouter();
+
+	const buildHref = (pageIndex: number) => {
+		params.set("page", pageIndex.toString());
+		return `${pathname}?${params.toString()}`;
+	};
+
 	return (
 		<div className="flex justify-between">
 			<div>
 				{data.total} {"Result(s)"}
 			</div>
-			<div>
-				{data.page} / {data.pages}
-			</div>
+			<ReactPaginate
+				activeLinkClassName="bg-primary text-white"
+				breakClassName="flex items-center px-3 py-3 text-gray-300"
+				containerClassName="flex flex-row"
+				disableInitialCallback={true}
+				hrefBuilder={buildHref}
+				initialPage={data.page - 1}
+				marginPagesDisplayed={2}
+				nextLabel={">"}
+				nextLinkClassName="flex items-center px-3 py-3 text-gray-300"
+				onPageChange={({ selected }) => {
+					push(buildHref(selected + 1));
+				}}
+				pageCount={data.pages}
+				pageLinkClassName="border-1 border-gray-100 flex items-center mx-1 min-w-10 px-2 py-2 rounded-1 text-gray-500 justify-center"
+				pageRangeDisplayed={2}
+				previousLabel={"<"}
+				previousLinkClassName="flex items-center px-3 py-2 text-gray-300"
+			/>
 			<Select>
 				<SelectTrigger>{`sort by: ${sort[0]!}`}</SelectTrigger>
 				<Popover>
